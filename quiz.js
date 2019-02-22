6d9b562ccb3bee5799b758f8804a05f9d6447763
Sam Kfoury <you@example.com>
2019-02-21 21:15:26 -0500

    added files

---
+++ quiz.js
@@ -0,0 +1,62 @@
+var score = 0;
+
+function submitAnswer() {
+    var radios = document.getElementsByName("choice");
+    var i = 0, len = radios.length;
+    var checked = false;
+    var userAnswer;
+
+    for (; i < len; i++) {
+        if (radios[i].checked) {
+            checked = true;
+            userAnswer = radios[i].value;
+        }
+    }
+    if (userAnswer === "Correct1") {
+        score++;
+    }
+
+    var radios = document.getElementsByName("choice2");
+    var i = 0, len = radios.length;
+    var checked = false;
+    var userAnswer;
+
+    for (; i < len; i++) {
+        if (radios[i].checked) {
+            checked = true;
+            userAnswer = radios[i].value;
+        }
+    }
+    if (userAnswer === "Correct2") {
+        score++;
+    }
+
+    var radios = document.getElementsByName("choice3");
+    var i = 0, len = radios.length;
+    var checked = false;
+    var userAnswer;
+
+    for (; i < len; i++) {
+        if (radios[i].checked) {
+            checked = true;
+            userAnswer = radios[i].value;
+        }
+    }
+    if (userAnswer === "Correct3") {
+        score++;
+        alert("Your score is: " + score)
+    }
+    else {
+        alert("Your score is: " + score)
+    }
+}
+var timeleft = 60;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished"
+    document.getElementById("finish").click();
+  }
+}, 1000);
