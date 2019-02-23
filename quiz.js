97649effcfe70e05efa164935d627c3b9b44801e
Sam Kfoury <you@example.com>
2019-02-23 12:05:55 -0500

    bugfix

--- quiz.js
+++ quiz.js
@@ -1,5 +1,16 @@
 var score = 0;
 
+var timeleft = 60;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Finished"
+    document.getElementById("finish").click();
+  }
+}, 1000);
+
 function submitAnswer() {
     var radios = document.getElementsByName("choice");
     var i = 0, len = radios.length;
@@ -44,19 +55,13 @@ function submitAnswer() {
     }
     if (userAnswer === "Correct3") {
         score++;
-        alert("Your score is: " + score)
+        alert("Your score is: " + score);
+        clearInterval(downloadTimer);        
+        document.getElementById("finish").style.visibility = "hidden"; 
     }
     else {
-        alert("Your score is: " + score)
+        alert("Your score is: " + score);
+        clearInterval(downloadTimer);        
+        document.getElementById("finish").style.visibility = "hidden"; 
     }
 }
-var timeleft = 60;
-var downloadTimer = setInterval(function(){
-  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-  timeleft -= 1;
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "Finished"
-    document.getElementById("finish").click();
-  }
-}, 1000);
